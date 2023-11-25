05fb6253ee1d77bb7647fed28833cd195ffed45f
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-25 15:54:41 -0500

    add authentication middleware

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
 //connectDB
-const connectDB = require("./db/connect")
-
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 //routes
-const authRouter = require("./routes/auth")
-const dataRouter = require("./routes/data")
+const authRouter = require("./routes/auth");
+const dataRouter = require("./routes/data");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/data", dataRouter);
+app.use("/api/v1/data", authenticateUser, dataRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -28,7 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
--- controllers/data.js
+++ controllers/data.js
@@ -1,27 +1,27 @@
-const getAllData = async(req,res) => {
-    res.send("get all data")
-}
+const getAllData = async (req, res) => {
+  res.send("get all data");
+};
 
-const getData = async(req,res) => {
-    res.send("get single data")
-}
+const getData = async (req, res) => {
+  res.send("get single data");
+};
 
-const createData = async(req,res) => {
-    res.send("create data")
-}
+const createData = async (req, res) => {
+  res.json(req.user);
+};
 
-const updateData = async(req,res) => {
-    res.send("update data")
-}
+const updateData = async (req, res) => {
+  res.send("update data");
+};
 
-const deleteData = async(req,res) => {
-    res.send("delete data")
-}
+const deleteData = async (req, res) => {
+  res.send("delete data");
+};
 
 module.exports = {
-    getAllData,
-     getData,
-     createData,
-     updateData,
-     deleteData
-    }
+  getAllData,
+  getData,
+  createData,
+  updateData,
+  deleteData
+};
