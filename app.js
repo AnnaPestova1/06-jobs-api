4e884f5acb49708f5da43a2576dd2cb2c84c21f6
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-12-03 16:58:18 -0500

    fixed route error

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Data API</h1><a href='/api-docs'>Documentation</a>");
 });
 
-app.use("api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/data", authenticateUser, dataRouter);
