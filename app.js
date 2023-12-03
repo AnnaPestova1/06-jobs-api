9374a772df0473fc27e0e3046ad7dfc648784081
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-12-03 16:52:13 -0500

    add swagger

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -21,6 +26,7 @@ const dataRouter = require("./routes/data");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(express.json());
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -28,14 +34,15 @@ app.use(
     max: 100 //limit each IP to 100 requests per windowMs
   })
 );
-app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("data api");
+  res.send("<h1>Data API</h1><a href='/api-docs'>Documentation</a>");
 });
+
+app.use("api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/data", authenticateUser, dataRouter);
