eb7460e74fb32bd69d3f5f3ebbe3198403b4118b
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-12-03 14:21:30 -0500

    add route

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req,res){
+  res.send("data api")
+})
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/data", authenticateUser, dataRouter);
